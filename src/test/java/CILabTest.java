74c4352f42a17e30202eb2fb8ddcb496f64504f2
kemeng001 <132250780+kemeng001@users.noreply.github.com>
2023-06-11 17:33:49 +0800

    Update CILabTest.java

--- src/test/java/CILabTest.java
+++ src/test/java/CILabTest.java
@@ -1,7 +1,8 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class CILabTest {
