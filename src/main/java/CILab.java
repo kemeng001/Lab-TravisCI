b3e1f855938697cd30f3abd1c3c5dc7c77fcedbc
kemeng001 <132250780+kemeng001@users.noreply.github.com>
2023-06-11 17:56:25 +0800

    Update CILab.java

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -11,22 +11,34 @@ public class CILab implements CILabInterface {
         myString = string;
     }
 
-    @Override
-    public boolean detectCapitalUse() {
-        if (myString == null) {
-            return false;
-        }
+@Override
+public boolean detectCapitalUse() {
+    if (myString == null) {
+        return false;
+    }
 
-        String[] words = myString.split(" ");
-        for (String word : words) {
-            if (!Character.isUpperCase(word.charAt(0)) || 
-                !word.substring(1).equals(word.substring(1).toLowerCase())) {
-                return false;
-            }
-        }
-
+    if (myString.equals(myString.toUpperCase())) {
         return true;
     }
+
+    String[] words = myString.split(" ");
+    for (String word : words) {
+        if (!Character.isUpperCase(word.charAt(0)) || 
+            !word.substring(1).equals(word.substring(1).toLowerCase())) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+
+
+
+
+
+
 }
 
 
